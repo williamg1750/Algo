e929b42d5ac85503cd2e3e390b074ec305b0b318
williamg1750 <william.guan12@gmail.com>
2021-04-22 10:18:07 -0400

    validAnagram Colt Steele

--- Leetcode.js
+++ Leetcode.js
@@ -2,6 +2,7 @@
 // Given two strings, write a function to determine if the second string is an anagram of the first.
 // An anagram is a word, phrase, or name formed by rearranging the letters of another]
 
+//my code
 function validAnagram(str1, str2) {
   // add whatever parameters you deem necessary - good luck!
   let hm1 = {};
@@ -25,5 +26,32 @@ function validAnagram(str1, str2) {
     if (!hm2[key]) return false;
     if (hm1[key] !== hm2[key]) return false;
   }
+  return true;
+}
+//colt steel code
+function validAnagram2(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  console.log(lookup);
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
   return true;
 }
