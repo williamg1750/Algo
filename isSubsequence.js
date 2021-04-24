b669a0c299debc938202ec924ae72b80c3da71ab
williamg1750 <william.guan12@gmail.com>
2021-04-24 11:44:17 -0400

    isSubsequence my answer

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,2 +1,9 @@
 //isSubsequence
-//testtest
+function isSubsequence(str1, str2) {
+  let j = 0;
+  for (let i = 0; i < str2.length; i++) {
+    if (str1[j] === str2[i]) j++;
+    if (j === str1.length) return true;
+  }
+  return false;
+}
