12cac77c60f0e33f4dec73086e106795fc6ea859
williamg1750 <william.guan12@gmail.com>
2021-04-24 11:48:28 -0400

    isSubsequence colt steele

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,4 +1,8 @@
 //isSubsequence
+// Given two strings s and t, check if s is a subsequence of t.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
 function isSubsequence(str1, str2) {
   let j = 0;
   for (let i = 0; i < str2.length; i++) {
@@ -7,3 +11,24 @@ function isSubsequence(str1, str2) {
   }
   return false;
 }
+
+//colt steele
+// isSubsequence Solution - Iterative
+// function isSubsequence(str1, str2) {
+//   var i = 0;
+//   var j = 0;
+//   if (!str1) return true;
+//   while (j < str2.length) {
+//     if (str2[j] === str1[i]) i++;
+//     if (i === str1.length) return true;
+//     j++;
+//   }
+//   return false;
+// }
+// isSubsequence Solution - Recursive but not O(1) Space
+// function isSubsequence(str1, str2) {
+//   if(str1.length === 0) return true
+//   if(str2.length === 0) return false
+//   if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+//   return isSubsequence(str1, str2.slice(1))
+// }
