56a7046f13e679e1600a0628aa03c388ccb4e305
williamg1750 <william.guan12@gmail.com>
2021-04-26 18:48:00 -0400

    nestedEvenSum solution

--- coltSteele/recursion/nestedEvenSum.js
+++ coltSteele/recursion/nestedEvenSum.js
@@ -34,3 +34,16 @@ var obj2 = {
 
 nestedEvenSum(obj1); // 6
 nestedEvenSum(obj2); // 10
+
+
+// nestedEvenSum Solution
+
+function nestedEvenSum (obj, sum=0) {
+    for (var key in obj) {
+        if (typeof obj[key] === 'object'){
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+            sum += obj[key];
+        }
+    }
+    return sum;
