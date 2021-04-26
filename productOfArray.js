0e7d33373a884076f619926cc8fc989700fb2383
williamg1750 <william.guan12@gmail.com>
2021-04-25 20:48:16 -0400

    productOfArray recursion

---
+++ productOfArray.js
@@ -0,0 +1,12 @@
+//productOfArray recursion
+
+// Write a function called&nbsp;<code>productOfArray</code>&nbsp;which takes in
+// an array of numbers and returns the product of them all.
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+const productOfArray = (arr) => {
+  if (!arr.length) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
+};
