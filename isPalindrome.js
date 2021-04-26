0146e14cb8f636c1c11aa68f3cf7a96155411a7f
williamg1750 <william.guan12@gmail.com>
2021-04-25 21:47:17 -0400

    isPalindrome Solution

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,4 +1,4 @@
-//isPalindrome
+//isPalindrome recursion
 
 // Write a recursive function called <strong>isPalindrome</strong> which returns
 // true if the string passed to it is a palindrome (reads the same forward and
@@ -21,4 +21,13 @@ function isPalindrome(string) {
   };
   helperfunc(string);
   return reversed === ogWord;
+}
+
+// isPalindrome Solution
+
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
 }
