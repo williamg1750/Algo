21634f8271f13824c83c639cd8ab31c2ff4819e2
williamg1750 <william.guan12@gmail.com>
2021-04-27 11:14:04 -0400

    linearSearchSudo code

--- coltSteele/linearSearch/linearSearchSudo.js
+++ coltSteele/linearSearch/linearSearchSudo.js
@@ -8,3 +8,14 @@
 //if not === value i++
 //if equal return i which is the index
 //return -1 at the bottom of the fucntion is it doesnt contain vlaue
+
+function linearSearch(arr, target) {
+  // add whatever parameters you deem necessary - good luck!
+  for (let i = 0; i < arr.length; i++) {
+    if (target === arr[i]) return i;
+  }
+  return -1;
+}
+
+//time complex is 0(n)
+//space is 0
