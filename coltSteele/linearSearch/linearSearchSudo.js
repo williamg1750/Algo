facd2753b881904f62eef595c4179f51a5bf4a14
williamg1750 <william.guan12@gmail.com>
2021-04-27 11:10:56 -0400

    linearSearchSudo sudo code

---
+++ coltSteele/linearSearch/linearSearchSudo.js
@@ -0,0 +1,10 @@
+//linearSearchSudo
+
+//sudo code
+
+//function that takes a value to search for
+//forloop looping thru the index
+//compair the value to the arr[i] with the ===
+//if not === value i++
+//if equal return i which is the index
+//return -1 at the bottom of the fucntion is it doesnt contain vlaue
