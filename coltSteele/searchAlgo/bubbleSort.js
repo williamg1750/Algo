b2c342745fbcbac791423f4fac45b02d7c0777db
williamg1750 <william.guan12@gmail.com>
2021-05-03 17:39:28 -0400

    selection sort

--- coltSteele/searchAlgo/bubbleSort.js
+++ coltSteele/searchAlgo/bubbleSort.js
@@ -17,6 +17,7 @@ const bubbleSort = (arr) => {
 };
 //[7,3,6,2,5,9,1,4,8]
 
+//better opt
 const bubbleSort = (arr) => {
   let temp;
   for (let i = arr.length - 1; i > 0; i--) {
@@ -27,6 +28,25 @@ const bubbleSort = (arr) => {
         arr[j] = temp;
       }
     }
+  }
+  return arr;
+};
+
+//even better opt fo nearly sorted data set input
+const bubbleSort = (arr) => {
+  let temp;
+  let noSwaps;
+  for (let i = arr.length - 1; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
   }
   return arr;
 };
---
+++ coltSteele/searchAlgo/selectionSort.js
@@ -0,0 +1,43 @@
+//selection sort
+
+//pseudo code
+
+// make a fuc that takes in a arr
+// make a for loop and make a variable calle min and set it to the arr[0]
+//start looping to the end and find you first min and swap with the first idx
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+  return arr;
+};
+
+//[2,4,3,1,7,8,5]
+//       M
+
+// ES2015 VERSION
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+
+  return arr;
+}
