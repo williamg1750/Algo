29f8fddcb34f68434dfabdb5a65e238d5dc4b74f
williamg1750 <william.guan12@gmail.com>
2021-05-03 16:13:52 -0400

    bubble sort naive

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,19 @@
 //bubble sort
-//pseudo
-//
+//pseudocode
 
-const bubbleSort = ();
+//naive answer
+const bubbleSort = (arr) => {
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+//[7,3,6,2,5,9,1,4,8]
