7e3a406fc60184309760b1302ffdffa044c620a9
williamg1750 <william.guan12@gmail.com>
2021-05-03 16:22:50 -0400

    bubble sort better

--- bubbleSort.js
+++ bubbleSort.js
@@ -15,5 +15,18 @@ const bubbleSort = (arr) => {
   }
   return arr;
 };
-
 //[7,3,6,2,5,9,1,4,8]
+
+const bubbleSort = (arr) => {
+  let temp;
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
