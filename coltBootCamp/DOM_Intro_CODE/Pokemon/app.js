2e617b9ca35b6e33182371ac666f02332b34caa6
williamg1750 <william.guan12@gmail.com>
2021-05-25 09:57:55 -0400

    pokemon DOM

--- coltBootCamp/DOM_Intro_CODE/Pokemon/app.js
+++ coltBootCamp/DOM_Intro_CODE/Pokemon/app.js
@@ -1,1 +1,21 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+let container = document.querySelector('#container');
+
+// const newImg = document.createElement('img');
+// newImg.src =
+//   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+
+// container.append(newImg);
+
+for (let i = 1; i <= 151; i++) {
+  const pokemon = document.createElement('div');
+  pokemon.classList.add('pokemon');
+  const label = document.createElement('span');
+  label.innerText = i;
+  const newImg = document.createElement('img');
+  newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+  pokemon.append(newImg);
+  pokemon.append(label);
+  container.append(pokemon);
+}
