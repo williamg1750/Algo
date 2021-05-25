03de3e076e13a9cbbad85201aa852dbfe51a7b62
williamg1750 <william.guan12@gmail.com>
2021-05-25 11:41:36 -0400

    add event listener

---
+++ coltBootCamp/DOM_Events_CODE/EventBasics/app.js
@@ -0,0 +1,58 @@
+// const btn = document.querySelector('#v2');
+
+// btn.onclick = function () {
+//     console.log("YOU CLICKED ME!")
+//     console.log("I HOPE IT WORKED!!")
+// }
+
+// function scream() {
+//     console.log("AAAAAHHHHH");
+//     console.log("STOP TOUCHING ME!")
+// }
+
+// btn.onmouseenter = scream;
+
+// document.querySelector('h1').onclick = () => {
+//     alert('you clicked the h1!')
+// }
+
+// const btn3 = document.querySelector('#v3');
+// btn3.addEventListener('click', function () {
+//     alert("CLICKED!");
+// })
+
+// function twist() {
+//     console.log("TWIST!")
+// }
+// function shout() {
+//     console.log("SHOUT!")
+// }
+
+// const tasButton = document.querySelector('#tas');
+
+// // tasButton.onclick = twist;
+// // tasButton.onclick = shout;
+
+// tasButton.addEventListener('click', twist)
+// tasButton.addEventListener('click', shout)
+const btn = document.querySelector('#v2');
+
+btn.onclick = function () {
+  console.log('you clicked me!!!!');
+  console.log('i hope it works');
+};
+
+function scream() {
+  console.log('AAAAHHHHHHHHH!!!!');
+  console.log('STOP IT');
+}
+
+btn.onmouseenter = scream;
+
+const btn3 = document.querySelector('#v3');
+
+btn3.addEventListener('click', () => {
+  console.log('u clicked btn 3');
+});
+
+const tasBtn = document.querySelector('#twistandshout');
